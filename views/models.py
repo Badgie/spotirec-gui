6a48bd8bb7fe9ae2f20ff29adf3a6aa0215da3bd
Badgy <jonaskhansen@gmail.com>
2020-05-05 16:34:11 +0200

    add some available parameters

--- views/models.py
+++ views/models.py
@@ -4,9 +4,10 @@ from .static import THEME
 
 class SpotirecButton(Button):
 
-    def __init__(self, text: str, button_color=(THEME['BACKGROUND'], THEME['BACKGROUND']),
-                 button_path='assets/img/elems/buttons/button-150x40.png',
-                 border_width=0, font=('', 13)):
-        super(SpotirecButton, self).__init__(text, image_filename=button_path,
-                                             border_width=border_width, button_color=button_color,
-                                             font=font)
+    def __init__(self, text: str, button: str = 'green', dims='150x40', key=None,
+                 button_color=(THEME['BACKGROUND'], THEME['BACKGROUND']),
+                 border_width=0, font=('', 13), visible=True):
+        super(SpotirecButton, self).__init__(
+            text, image_filename=f'assets/img/elems/buttons/button-{button}-{dims}.png',
+            border_width=border_width, button_color=button_color, font=font, key=key,
+            visible=visible)
